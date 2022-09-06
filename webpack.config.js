7dfd967450fd465e5bda8e8d07f8f606f6bb5952
Berk Şengül <berksengul17@gmail.com>
2022-09-06 14:57:18 +0300

    Add basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|tff|eot|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
