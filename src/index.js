63fa3938f6e9a028fed09487008d0cd41130225f
Berk Şengül <berksengul17@gmail.com>
2022-09-07 15:36:32 +0300

    Add image to html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.css";
+import logo from "./images/logo.png";
+
+const header = document.querySelector("header");
+
+const myLogo = new Image();
+myLogo.classList.add("logo");
+myLogo.src = logo;
+
+header.prepend(myLogo);
