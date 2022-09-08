1c2cc72141dbff935e635001c38b89eb921e34ea
Berk Şengül <berksengul17@gmail.com>
2022-09-08 12:01:06 +0300

    Import main page load

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import "./style.css";
-import logo from "./images/logo.png";
+import loadPage from "./modules/mainPage.js";
 
-const header = document.querySelector("header");
-
-const myLogo = new Image();
-myLogo.classList.add("logo");
-myLogo.src = logo;
-
-header.prepend(myLogo);
+loadPage();
