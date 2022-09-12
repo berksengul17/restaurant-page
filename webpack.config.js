4e8ccb644c04a0b0905f2b47e05ab302e495f882
Berk Şengül <berksengul17@gmail.com>
2022-09-12 22:14:38 +0300

    Configure to use mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,7 @@ module.exports = {
       title: "MC Restaurant",
       template: "./src/index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
   output: {
     filename: "main.js",
@@ -23,7 +25,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
