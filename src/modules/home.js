889e92d985d7d0b42b85e298388368331ddd1676
Berk Şengül <berksengul17@gmail.com>
2022-09-15 15:12:45 +0300

    Seperate menuIcon html and home html

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,22 +1,7 @@
-import "../styles/home.css";
-import menuIcon from "../images/goldenApple.gif";
-
 function loadHome() {
-  const content = document.querySelector("#content");
-
-  const menuContainer = document.createElement("div");
-  const para = document.createElement("p");
-  const myIcon = new Image();
-
-  myIcon.src = menuIcon;
-  para.textContent = "MENU";
-
-  menuContainer.classList.add("menu-container");
-
-  menuContainer.appendChild(para);
-  menuContainer.appendChild(myIcon);
-
-  content.appendChild(menuContainer);
+  // Display the menu icon which is at the bottom right
+  const menuContainer = document.querySelector(".menu-container");
+  menuContainer.style = "visibility:visible";
 }
 
 export default loadHome;
